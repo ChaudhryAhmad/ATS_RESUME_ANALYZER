8ca511eb9a4a2d16c95cee8ce332d6fd665a78df
ChaudhryAhmadMubashar <chahmadmubashir0@gmail.com>
2026-08-23 14:16:39 +0300

    Just checking 2

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,6 +28,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://ats-resume-analyzer-five-delta.vercel.app",
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
