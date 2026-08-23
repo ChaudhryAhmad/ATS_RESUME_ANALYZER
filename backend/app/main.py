8cb718ef28c4391b0991af042893e5e5cb007468
ChaudhryAhmadMubashar <chahmadmubashir0@gmail.com>
2026-08-23 12:55:19 +0300

    Changing main file

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://your-app.vercel.app",
+        "https://ats-resume-analyzer-five-delta.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
